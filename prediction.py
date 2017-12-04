e2faf45ad9d6a54f9ecd85f2994536b229fdf6ec
watanabeyyy <yuki.watanabe@furukawaelectric.com>
2017-12-04 10:51:44 +0900

    update

--- prediction.py
+++ prediction.py
@@ -1,23 +1,27 @@
 import mvnc.mvncapi as mvnc
 import numpy as np
 
+
 def predict(input):
     devices = mvnc.EnumerateDevices()
     device = mvnc.Device(devices[0])
     device.OpenDevice()
 
-    with open('graph', 'rb') as f:
+    with open('./model/graph', 'rb') as f:
         blob = f.read()
     graph = device.AllocateGraph(blob)
 
-    graph.LoadTensor(input.astype(np.float16), 'user object')
-    output, userobj = graph.GetResult()
+    for i in range(4):
+        graph.LoadTensor(input[i], 'user object')
+        output, userobj = graph.GetResult()
+        print(np.argmax(output))
 
     graph.DeallocateGraph()
     device.CloseDevice()
 
     return output
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     input = np.load("test_data.npy")
-    print(np.argmax(predict(input)))
+    predict(input.astype(np.float16))
